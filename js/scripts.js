fc169168c0994631aae3c57d50c0af87ffc65860
Duke Yin <yduke@hotmail.com>
2020-09-16 22:11:46 +0800

    badge for drafts count

--- js/scripts.js
+++ js/scripts.js
@@ -14,68 +14,6 @@ const bodyInput = document.querySelector('#content');
 const form = document.querySelector('#postform');
 const submitBtn = document.querySelector('form button');
 
-//function to Display databse data
-// Define the displayData() function
-function displayData() {
-  // Here we empty the contents of the list element each time the display is updated
-  // If you didn't do this, you'd get duplicates listed each time a new note is added
-  while (list.firstChild) {
-    list.removeChild(list.firstChild);
-  }
-
-  // Open our object store and then get a cursor - which iterates through all the
-  // different data items in the store
-  let objectStore = db.transaction('notes_os').objectStore('notes_os');
-  objectStore.openCursor().onsuccess = function(e) {
-    // Get a reference to the cursor
-    let cursor = e.target.result;
-
-    // If there is still another data item to iterate through, keep running this code
-    if(cursor) {
-      // Create a list item, h3, and p to put each data item inside when displaying it
-      // structure the HTML fragment, and append it inside the list
-      const listItem = document.createElement('li');
-      const h3 = document.createElement('h3');
-      const para = document.createElement('p');
-
-      listItem.appendChild(h3);
-      listItem.appendChild(para);
-      list.appendChild(listItem);
-
-      // Put the data from the cursor inside the h3 and para
-      h3.textContent = cursor.value.title;
-      para.textContent = cursor.value.body;
-
-      // Store the ID of the data item inside an attribute on the listItem, so we know
-      // which item it corresponds to. This will be useful later when we want to delete items
-      listItem.setAttribute('data-note-id', cursor.value.id);
-
-      // Create a button and place it inside each listItem
-      const deleteBtn = document.createElement('button');
-      listItem.appendChild(deleteBtn);
-      deleteBtn.textContent = 'Delete';
-		deleteBtn.classList.add("btn","waves-effect","waves-light");
-
-
-      // Set an event handler so that when the button is clicked, the deleteItem()
-      // function is run
-      deleteBtn.onclick = deleteItem;
-
-      // Iterate to the next item in the cursor
-      cursor.continue();
-    } else {
-      // Again, if list item is empty, display a 'No notes stored' message
-      if(!list.firstChild) {
-        const listItem = document.createElement('li');
-        listItem.textContent = 'No dafts stored.';
-        list.appendChild(listItem);
-      }
-      // if there are no more cursor items to iterate through, say so
-      console.log('Notes all displayed');
-    }
-  };
-}
-
 
 //IndexedDB
 let db;
@@ -196,13 +134,14 @@ window.onload = function() {
         // Create a button and place it inside each listItem
         const deleteBtn = document.createElement('button');
         listItem.appendChild(deleteBtn);
-        deleteBtn.textContent = 'Delete';
-        deleteBtn.classList.add("btn","waves-effect","waves-light");
+        deleteBtn.textContent = 'Del';
+        deleteBtn.classList.add("btn-floating","waves-effect","waves-light");
 
         // Set an event handler so that when the button is clicked, the deleteItem()
         // function is run
         deleteBtn.onclick = deleteItem;
-
+		
+		document.querySelector('#daftcount').innerHTML = document.getElementById('draftbox').childNodes.length;
         // Iterate to the next item in the cursor
         cursor.continue();
       } else {
